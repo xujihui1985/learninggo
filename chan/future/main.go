749ad0dd21a6cdf26451ebaf0487cfeccc40952b
慕陶 <jihui.xjh@alibaba-inc.com>
2018-10-30 15:42:27 +0800

    update

--- chan/future/main.go
+++ chan/future/main.go
@@ -13,8 +13,11 @@ func withFuture(ca, cb string) {
 }
 
 func main(){
+
+	// this is ok, but not ideal, because fa and fb will be invoke sequencely
 	withFuture(<-fa(), <-fb())
 
+	// this is good because fa and fb are invoke concurrently
 	ca, cb := fa(), fb()
 	withFuture(<-ca, <-cb)
 }
